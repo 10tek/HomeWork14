333d1ad29f7ef6689d02c89eaa51ae16585b7af7
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-29 17:24:54 +0600

    Добавил класс Player для игрока

---
+++ HW14/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public List<Karta> Cards { get; set; }
+
+        public void ShowCards()
+        {
+            foreach(var card in Cards)
+            {
+                Console.WriteLine($"{card.Suit} - {card.CardType}");
+            }
+        }
+    }
+}
