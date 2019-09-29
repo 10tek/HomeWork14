8f916caf27dd5029e9bb23f28387b2ead3936bc1
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-29 17:21:18 +0600

    Добавил класс Karta и перечисления

---
+++ HW14/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    public enum Enums
+    {
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
+    }
+
+    public enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades,
+    }
+}
---
+++ HW14/Karta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    class Karta
+    {
+        public Suit Suit { get; set; }
+        public Enums CardType { get; set; }
+    }
+}
